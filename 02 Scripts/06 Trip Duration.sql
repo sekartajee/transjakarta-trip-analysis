53b12f256d3d2986d35b77adf8338a8dc6c99190
Sekartajee <88395163+sekartajee@users.noreply.github.com>
2026-02-03 04:12:56 +0700

    Update 06 Trip Duration.sql

--- 02 Scripts/06 Trip Duration.sql
+++ 02 Scripts/06 Trip Duration.sql
@@ -13,6 +13,7 @@ WHERE tc.tapInTime IS NOT NULL AND tc.tapOutTime IS NOT NULL;
 
 -- 2. Trip Duration Summary per Corridor
 SELECT 
+	COALESCE(dc.corridorID) AS corrID,
     COALESCE(tc.corridorPK, 0) AS corridorPK,
     COALESCE(dc.corridorName, 'Unknown Corridor') AS corridorName,
     MIN(TIMESTAMPDIFF(MINUTE, tc.tapInTime, tc.tapOutTime)) AS min_duration,
@@ -24,6 +25,32 @@ LEFT JOIN dim_corridor dc ON tc.corridorPK = dc.corridorPK
 WHERE tc.tapInTime IS NOT NULL AND tc.tapOutTime IS NOT NULL
 GROUP BY tc.corridorPK, dc.corridorName
 ORDER BY avg_duration DESC;
+
+
+
+-- 3. Trip Duration Summary per Corridor with Rush Hour Averages
+SELECT 
+	COALESCE(dc.corridorID) AS corrID,
+    COALESCE(tc.corridorPK, 0) AS corridorPK,
+    COALESCE(dc.corridorName, 'Unknown Corridor') AS corridorName,
+    MIN(TIMESTAMPDIFF(MINUTE, tc.tapInTime, tc.tapOutTime)) AS min_duration,
+    MAX(TIMESTAMPDIFF(MINUTE, tc.tapInTime, tc.tapOutTime)) AS max_duration,
+    AVG(TIMESTAMPDIFF(MINUTE, tc.tapInTime, tc.tapOutTime)) AS avg_duration,
+    STDDEV(TIMESTAMPDIFF(MINUTE, tc.tapInTime, tc.tapOutTime)) AS std_duration,
+    AVG(CASE 
+            WHEN HOUR(tc.tapInTime) BETWEEN 5 AND 7 
+            THEN TIMESTAMPDIFF(MINUTE, tc.tapInTime, tc.tapOutTime) 
+        END) AS morning_rush_avg,
+    AVG(CASE 
+            WHEN HOUR(tc.tapInTime) BETWEEN 16 AND 18 
+            THEN TIMESTAMPDIFF(MINUTE, tc.tapInTime, tc.tapOutTime) 
+        END) AS evening_rush_avg
+FROM trip_complete tc
+LEFT JOIN dim_corridor dc ON tc.corridorPK = dc.corridorPK
+WHERE tc.tapInTime IS NOT NULL AND tc.tapOutTime IS NOT NULL
+GROUP BY tc.corridorPK, dc.corridorName
+ORDER BY evening_rush_avg asc;
+
 
 
 
